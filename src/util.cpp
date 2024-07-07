366dfa6821f3b49ded1f6b99c17a1ada5e80300a
Abu Sayeed Roni <roni@dal.ca>
2024-07-07 01:03:10 -0300

    Add print utilities

--- src/util.cpp
+++ src/util.cpp
@@ -1,1 +1,36 @@
+#include <iomanip>
 #include "util.h"
+
+std::ostream& operator<<(std::ostream& os, const IPrintable& printable) {
+    printable.print(os);
+    return os;
+}
+
+FormattedTime::FormattedTime() : hours(0), minutes(0), seconds(0), milliseconds(0), microseconds(0) {}
+    
+FormattedTime::FormattedTime(int64_t microseconds) {
+    int64_t temp = microseconds;
+    
+    this->microseconds = temp % 1000;
+    temp /= 1000;
+    
+    milliseconds = temp % 1000;
+    temp /= 1000;
+
+    seconds = temp % 60;
+    temp /= 60;
+
+    minutes = temp % 60;
+    temp /= 60;
+
+    hours = temp;
+}
+
+void FormattedTime::print(std::ostream& os) const {
+    os << std::setfill('0');
+    os << std::setw(2) << hours << ":";
+    os << std::setw(2) << minutes << ":";
+    os << std::setw(2) << seconds << ".";
+    os << std::setw(3) << milliseconds;
+    os << std::setw(3) << microseconds;
+}
--- src/util.h
+++ src/util.h
@@ -4,9 +4,30 @@
 #include <iostream>
 
 template <typename T>
-inline void print(T obj, const char* end = "\n", bool flush_buffer = false) {
+inline void print(T obj, const std::string& end = "\n", bool flush_buffer = false) {
     std::cout << obj << end;
     if (flush_buffer) {
         std::cout.flush();
     }
 }
+
+class IPrintable {
+public:
+    virtual void print(std::ostream& os) const = 0;
+    virtual ~IPrintable() = default;
+};
+
+std::ostream& operator<<(std::ostream& os, const IPrintable& printable);
+
+struct FormattedTime : public IPrintable {
+    int32_t hours;
+    int8_t minutes;
+    int8_t seconds;
+    int16_t milliseconds;
+    int16_t microseconds;
+
+    FormattedTime();
+    explicit FormattedTime(int64_t microseconds);
+
+    void print(std::ostream& os) const override;
+};
