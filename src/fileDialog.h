2378c72da6e01b34eee9e3c4066299f686ee0226
Abu Sayeed Roni <roni@dal.ca>
2024-07-10 14:45:13 -0300

    add callback to the file dialogs

--- src/fileDialog.h
+++ src/fileDialog.h
@@ -5,5 +5,24 @@
 #include <string>
 #include <SDL2/SDL.h>
 
-std::future<std::optional<std::string>> openFileDialogAsync(bool directory = false);
-std::future<std::optional<std::string>> saveFileDialogAsync(SDL_Window* parent_window);
+/**
+ * @brief Asynchronously opens a file or folder selection dialog.
+ *
+ * @param callback A function that will be called with the selected file or folder path,
+ * wrapped in std::optional<std::string>. If no file or folder is selected, std::nullopt will be passed.
+ 
+ * @param directory If true, the dialog will allow selecting directories; otherwise, it will 
+ * allow selecting files. Default is false.
+ */
+void openFileDialogAsync(std::function<void(std::optional<std::string>)> callback, bool directory = false);
+
+/**
+ * @brief Asynchronously opens a save file dialog.
+ * 
+ * @param callback A function that will be called with the selected file path,
+ * wrapped in std::optional<std::string>. If no file is selected, std::nullopt will be passed.
+ * 
+ * @param parent_window The SDL_Window* that represents the parent window. The save dialog
+ * will be modal to this window.
+ */
+void saveFileDialogAsync(std::function<void(std::optional<std::string>)> callback, SDL_Window* parent_window);
--- src/fileDialog.mm
+++ src/fileDialog.mm
@@ -4,14 +4,10 @@
 #include <SDL2/SDL_syswm.h>
 #include <AppKit/AppKit.h>
 
-std::future<std::optional<std::string>> openFileDialogAsync(bool directory) {
+void openFileDialogAsync(std::function<void(std::optional<std::string>)> callback, bool directory) {
     
-    // Create a promise to return the file or folder path.
-    std::shared_ptr<std::promise<std::optional<std::string>>> promise = std::make_shared<std::promise<std::optional<std::string>>>();
-    auto future = promise->get_future();
-
     // Dispatch the file dialog to the main queue asynchronously.
-    dispatch_async(dispatch_get_main_queue(), [promise = std::move(promise), directory]() mutable {
+    dispatch_async(dispatch_get_main_queue(), [callback = std::move(callback), directory]() mutable {
         NSOpenPanel* panel = [NSOpenPanel openPanel];
         [panel setAllowsMultipleSelection:NO];
         [panel setCanChooseDirectories:directory];
@@ -23,26 +19,20 @@ std::future<std::optional<std::string>> openFileDialogAsync(bool directory) {
         if (result == NSModalResponseOK) {
             NSURL* selectedURL = [[panel URLs] firstObject];
             std::string path = std::string([[selectedURL path] UTF8String]);
-            promise->set_value(std::optional<std::string>{path});
+            callback(std::optional<std::string>{path});
         } else {
-            promise->set_value(std::nullopt);
+            callback(std::nullopt);
         }
     });
-
-    return future;
 }
 
-std::future<std::optional<std::string>> saveFileDialogAsync(SDL_Window* parent_window) {
+void saveFileDialogAsync(std::function<void(std::optional<std::string>)> callback, SDL_Window* parent_window) {
     
-    // Create a promise to return the file path.
-    std::shared_ptr<std::promise<std::optional<std::string>>> promise = std::make_shared<std::promise<std::optional<std::string>>>();
-    auto future = promise->get_future();
-
     // Dispatch the file dialog to the main queue asynchronously.
-    dispatch_async(dispatch_get_main_queue(), [promise = std::move(promise), parent_window]() mutable {
+    dispatch_async(dispatch_get_main_queue(), [callback = std::move(callback), parent_window]() mutable {
         NSSavePanel* panel = [NSSavePanel savePanel];
 
-        // Set properties of the save panel
+        // Set properties of the save panel.
         [panel setCanCreateDirectories:YES];
         [panel setShowsTagField:NO];
 
@@ -57,12 +47,10 @@ std::future<std::optional<std::string>> saveFileDialogAsync(SDL_Window* parent_w
             if (result == NSModalResponseOK) {
                 NSURL* selectedFile = [panel URL];
                 std::string filePath = std::string([[selectedFile path] UTF8String]);
-                promise->set_value(std::optional<std::string>{filePath});
+                callback(std::optional<std::string>{filePath});
             } else {
-                promise->set_value(std::nullopt);
+                callback(std::nullopt);
             }
         }];
     });
-
-    return future;
 }
